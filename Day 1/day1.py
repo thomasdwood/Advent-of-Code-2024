1c081ff3cb694c869f45c62fd0bd4bdba7e3cc8a
Tom Wood <thomas.d.wood@gmail.com>
2024-12-02 21:53:30 -0500

    Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+
+def splitem(line):
+    s = line.split(' ')
+    left = s[0]
+    right = s[-1]
+    return (int(left), int(right))
+
+def main():
+    distance = lambda a, b : abs(a-b)
+    with open('Day 1/input.txt') as f:
+        lines = f.readlines()
+    all_ids = list(map(splitem, lines))
+    left, right = map(list, zip(*all_ids))
+    left = sorted(left)
+    right = sorted(right)
+    total_distance = 0
+    for x, y in zip(left, right):
+        total_distance += distance(x, y)
+    print(total_distance)
+    similarity = 0
+    right_count = Counter(right)
+    for x in left:
+        if x in right:
+            similarity += x * right_count[x]
+
+    print(similarity)
+
+
+
+if __name__=="__main__":
+
+    main()
